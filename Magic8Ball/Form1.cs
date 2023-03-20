25ecd19fdd5aee14c17b569c7711fca3b0d2b876
xKronos58 <finleycrowther@outlook.com>
2023-03-20 17:52:21 +1100

    Initial Commit

--- Magic8Ball/Form1.cs
+++ Magic8Ball/Form1.cs
@@ -10,11 +10,11 @@ using System.Windows.Forms;
 
 namespace Magic8Ball
 {
-  public partial class Form1 : Form
-  {
-    public Form1()
+    public partial class Form1 : Form
     {
-      InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
+        }
     }
-  }
 }
