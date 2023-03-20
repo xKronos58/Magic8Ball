3a2ad5727e768504f751a954a88ba7c5db9af189
xKronos58 <finleycrowther@outlook.com>
2023-03-20 18:34:31 +1100

    Finished Project

--- Magic8Ball/Form1.Designer.cs
+++ Magic8Ball/Form1.Designer.cs
@@ -29,11 +29,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Answer = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Answer
+            // 
+            this.Answer.BackColor = System.Drawing.Color.Transparent;
+            this.Answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Answer.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Answer.Image = ((System.Drawing.Image)(resources.GetObject("Answer.Image")));
+            this.Answer.Location = new System.Drawing.Point(246, 57);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(164, 157);
+            this.Answer.TabIndex = 2;
+            this.Answer.Text = "Output";
+            this.Answer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Answer.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(643, 361);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(563, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Stuff?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(563, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 64);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Code";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(563, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(68, 64);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Fibonacci";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(563, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(68, 64);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Free Choice";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(641, 360);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Answer);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.Windows.Forms.Label Answer;
 
         #endregion
     }
--- Magic8Ball/Form1.cs
+++ Magic8Ball/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,73 @@ namespace Magic8Ball
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private string[] ans =
+        {
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
+        };
+        
+        //If the user clicks the background the ball rolls
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Answer.Visible = false; 
+            var rand = new Random();
+            Thread.Sleep(2000);
+            Answer.Visible = true;
+            Answer.BringToFront();
+            Answer.Text = ans[rand.Next(0, 20)];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!button2.Visible){
+                button2.Visible = true;
+                button3.Visible = true;
+                button4.Visible = true;
+                button1.Text = @"Hide?";
+            }
+            else
+            {
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Visible = false;
+                button1.Text = @"Stuff?";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/xKronos58/Magic8Ball.git");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/xKronos58/FibonacciApplication.git");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
